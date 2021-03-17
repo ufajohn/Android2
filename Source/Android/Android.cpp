d01abcfb25e2d6752bac37f921d3be308bfbc0f0
John <ufa-john@yandex.ru>
2021-03-17 12:25:13 +0500

    Initial commit

---
+++ Source/Android/Android.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Android.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Android, "Android" );
---
+++ Source/Android/AndroidGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AndroidGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANDROID_API AAndroidGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
